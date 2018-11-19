82ba7b899aef97022ef1605df39bf8a5d96766df
Dreamlu <3147460271@qq.com>
2018-11-19 15:02:30 +0800

    change some and add conf

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
-package main
-
-import (
-	"deercoder-gin/conf"
-	"deercoder-gin/routers"
-	_ "deercoder-gin/util/db"
-	"github.com/gin-gonic/gin"
-)
-
-func main() {
-	gin.SetMode(gin.DebugMode)
-	r := routers.SetRouter()
-	// Listen and Server in 0.0.0.0:8080
-	r.Run(":" + conf.GetConfigValue("http_port"))
-}
+package deercoder
+// main.go
+//import (
+//	"deercoder-gin/conf"
+//	"deercoder-gin/routers"
+//	_ "deercoder-gin/util/db"
+//	"github.com/gin-gonic/gin"
+//)
+//
+//func main() {
+//	gin.SetMode(gin.DebugMode)
+//	r := routers.SetRouter()
+//	// Listen and Server in 0.0.0.0:8080
+//	r.Run(":" + conf.GetConfigValue("http_port"))
+//}
