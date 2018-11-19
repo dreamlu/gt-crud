e2b89068e2f81229174a5cbfbd0e77a08d608e22
Dreamlu <3147460271@qq.com>
2018-11-19 20:03:11 +0800

    add GetUriValues

--- deercoder.go
+++ deercoder.go
@@ -1,4 +1,9 @@
 package deercoder
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
 // main.go
 //import (
 //	"github.com/Dreamlu/deercoder-gin/routers"
@@ -11,3 +16,12 @@ package deercoder
 //	// Listen and Server in 0.0.0.0:8080
 //	r.Run(":" + GetConfigValue("http_port"))
 //}
+
+
+// return values
+// type map[string][]string
+func GetUriValues(u *gin.Context) map[string][]string {
+	u.Request.ParseForm()
+	values := u.Request.Form //need to ParseForm
+	return values
+}
