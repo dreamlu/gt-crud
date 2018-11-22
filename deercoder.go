ada7e8c50c83797a9ab420949affae82897853c5
Dreamlu <3147460271@qq.com>
2018-11-22 21:17:42 +0800

    update

--- deercoder.go
+++ deercoder.go
@@ -4,20 +4,6 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// main.go
-//import (
-//	"github.com/Dreamlu/deercoder-gin/routers"
-//	"github.com/gin-gonic/gin"
-//)
-//
-//func main() {
-//	gin.SetMode(gin.DebugMode)
-//	r := routers.SetRouter()
-//	// Listen and Server in 0.0.0.0:8080
-//	r.Run(":" + GetConfigValue("http_port"))
-//}
-
-
 // return values
 // type map[string][]string
 func GetUriValues(u *gin.Context) map[string][]string {
