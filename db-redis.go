7c8b9f2402c079ec4bf4e79901257e662e90ca94
Dreamlu <3147460271@qq.com>
2018-11-28 16:10:55 +0800

    add redis

--- db-redis.go
+++ db-redis.go
@@ -1,1 +1,157 @@
 package deercoder
+
+import (
+	"github.com/gomodule/redigo/redis"
+	"log"
+	"time"
+)
+
+// ConnPool is RDS struct
+type ConnPool struct {
+	redisPool *redis.Pool
+}
+
+// InitRedisPool func init RDS fd
+func InitRedisPool(host, password string, database, maxOpenConns, maxIdleConns int) *ConnPool {
+	r := &ConnPool{}
+	r.redisPool = newPool(host, password, database, maxOpenConns, maxIdleConns)
+	if _, err := r.Do("PING"); err != nil {
+		log.Panicln("Init redis pool failed.", err.Error())
+	}
+	return r
+}
+
+func newPool(server, password string, database, maxOpenConns, maxIdleConns int) *redis.Pool {
+	return &redis.Pool{
+		MaxActive:   maxOpenConns, // max number of connections
+		MaxIdle:     maxIdleConns, // 最大的空闲连接数
+		IdleTimeout: 120 * time.Second,
+		Dial: func() (redis.Conn, error) {
+			c, err := redis.Dial("tcp", server)
+			if err != nil {
+				return nil, err
+			}
+			if len(password) > 0 {
+				if _, err := c.Do("AUTH", password); err != nil {
+					c.Close()
+					return nil, err
+				}
+			}
+			if _, err := c.Do("select", database); err != nil {
+				c.Close()
+				return nil, err
+			}
+			return c, err
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			_, err := c.Do("PING")
+			return err
+		},
+	}
+}
+
+// Close pool
+func (p *ConnPool) Close() error {
+	err := p.redisPool.Close()
+	return err
+}
+
+// Do commands
+func (p *ConnPool) Do(command string, args ...interface{}) (interface{}, error) {
+	conn := p.redisPool.Get()
+	defer conn.Close()
+	return conn.Do(command, args...)
+}
+
+// SetString for string
+func (p *ConnPool) SetString(key string, value interface{}) (interface{}, error) {
+	conn := p.redisPool.Get()
+	defer conn.Close()
+	return conn.Do("SET", key, value)
+}
+
+// GetString for string
+func (p *ConnPool) GetString(key string) (string, error) {
+	// get one connection from pool
+	conn := p.redisPool.Get()
+	// put connection to pool
+	defer conn.Close()
+	return redis.String(conn.Do("GET", key))
+}
+
+// GetBytes for bytes
+func (p *ConnPool) GetBytes(key string) ([]byte, error) {
+	conn := p.redisPool.Get()
+	defer conn.Close()
+	return redis.Bytes(conn.Do("GET", key))
+}
+
+// GetInt for int
+func (p *ConnPool) GetInt(key string) (int, error) {
+	conn := p.redisPool.Get()
+	defer conn.Close()
+	return redis.Int(conn.Do("GET", key))
+}
+
+// GetInt64 for int64
+func (p *ConnPool) GetInt64(key string) (int64, error) {
+	conn := p.redisPool.Get()
+	defer conn.Close()
+	return redis.Int64(conn.Do("GET", key))
+}
+
+// DelKey for key
+func (p *ConnPool) DelKey(key string) (interface{}, error) {
+	conn := p.redisPool.Get()
+	defer conn.Close()
+	return conn.Do("DEL", key)
+}
+
+// ExpireKey for key
+func (p *ConnPool) ExpireKey(key string, seconds int64) (interface{}, error) {
+	conn := p.redisPool.Get()
+	defer conn.Close()
+	return conn.Do("EXPIRE", key, seconds)
+}
+
+// Keys for key
+func (p *ConnPool) Keys(pattern string) ([]string, error) {
+	conn := p.redisPool.Get()
+	defer conn.Close()
+	return redis.Strings(conn.Do("KEYS", pattern))
+}
+
+// KeysByteSlices for key
+func (p *ConnPool) KeysByteSlices(pattern string) ([][]byte, error) {
+	conn := p.redisPool.Get()
+	defer conn.Close()
+	return redis.ByteSlices(conn.Do("KEYS", pattern))
+}
+
+// SetHashMap for hash map
+func (p *ConnPool) SetHashMap(key string, fieldValue map[string]interface{}) (interface{}, error) {
+	conn := p.redisPool.Get()
+	defer conn.Close()
+	return conn.Do("HMSET", redis.Args{}.Add(key).AddFlat(fieldValue)...)
+}
+
+// GetHashMapString for hash map
+func (p *ConnPool) GetHashMapString(key string) (map[string]string, error) {
+	conn := p.redisPool.Get()
+	defer conn.Close()
+	return redis.StringMap(conn.Do("HGETALL", key))
+}
+
+// GetHashMapInt for hash map
+func (p *ConnPool) GetHashMapInt(key string) (map[string]int, error) {
+	conn := p.redisPool.Get()
+	defer conn.Close()
+	return redis.IntMap(conn.Do("HGETALL", key))
+}
+
+// GetHashMapInt64 for hash map
+func (p *ConnPool) GetHashMapInt64(key string) (map[string]int64, error) {
+	conn := p.redisPool.Get()
+	defer conn.Close()
+	return redis.Int64Map(conn.Do("HGETALL", key))
+}
