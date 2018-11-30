7949e96dcf4de0e1577d93f07515951234992841
Dreamlu <3147460271@qq.com>
2018-11-30 17:01:27 +0800

    add xss

---
+++ util/xss/xss.go
@@ -0,0 +1,13 @@
+package xss
+
+import "html"
+
+//type Xss struct {
+//
+//}
+func XssMap(args map[string][]string) map[string][]string {
+	for _, v := range args {
+		v[0] = html.EscapeString(v[0])
+	}
+	return args
+}
