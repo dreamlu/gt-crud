bc68b7495d27cbeab4c0561b15169bddb27153b3
Dreamlu <3147460271@qq.com>
2018-11-30 12:19:30 +0800

    validator update

--- validator/validator.go
+++ validator/validator.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/Dreamlu/deercoder-gin/util/lib"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -15,7 +16,7 @@ type Validator struct {
 
 type vRule struct {
 	vr       ValidateRuler
-	required bool
+	//required bool
 }
 
 //校验规则接口，支持自定义规则
@@ -26,6 +27,7 @@ type ValidateRuler interface {
 //内置规则结构，实现ValidateRuler接口
 type normalRule struct {
 	key    string
+	trans  string //翻译后的字段名
 	rule   string
 	params string
 }
@@ -38,61 +40,97 @@ func NewValidator(data map[string]string) *Validator {
 }
 
 //添加内置的校验规则(同一个key只能有一条规则，重复添加会覆盖)
-func (this *Validator) AddRule(key string, rule string, params string, required ...bool) {
-	nr := &normalRule{key, rule, params}
-	this.rule[key] = &vRule{nr, true} //默认required = true
-	if len(required) > 0 {
-		this.rule[key].required = required[0]
-	}
+func (this *Validator) AddRule(key string, trans, rule string, params string) {
+	nr := &normalRule{key, trans, rule, params}
+	this.rule[key] = &vRule{nr}//, true} //默认required = true
 }
 
 //框架不可能包括所有的规则，为了满足不同应用的需要，除了内置规则外，需同时支持自定义规则的添加。
 //go不支持重载，所以定义一个新方法来添加自定义规则（使用ValidateRuler interface参数）
 func (this *Validator) AddExtRule(key string, rule ValidateRuler, required ...bool) {
-	this.rule[key] = &vRule{rule, true}
-	if len(required) > 0 {
-		this.rule[key].required = required[0]
+	this.rule[key] = &vRule{rule} //, true}
+	//if len(required) > 0 {
+	//	this.rule[key].required = required[0]
+	//}
+}
+
+// 执行检查后返回信息
+// trans 翻译后的字段名
+func (this *Validator) CheckInfo(trans string) interface{} {
+	if err := this.Check(); err != nil {
+		//检查不通过，处理错误
+		//fmt.Println(err)
+		//return err
+		for k, _ := range this.rule {
+			if err[k] != nil{
+				return lib.GetMapData(lib.CodeValidator,err[k])
+			}
+		}
 	}
+	return nil
 }
 
 //执行检查
 func (this *Validator) Check() (errs map[string]error) {
+
 	errs = make(map[string]error)
 	for k, v := range this.rule {
-		data, exists := this.data[k]
-		if !exists { //无值
-			if v.required { //如果必填，报错
-				errs[k] = errors.New("data error: required field miss")
-			}
-		} else { //有值判断规则
-			if err := v.vr.Check(data); err != nil { //调用ValidateRuler接口的Check方法来检查
-				errs[k] = err
-			}
+		data, _ := this.data[k]
+		if err := v.vr.Check(data); err != nil { //调用ValidateRuler接口的Check方法来检查
+			errs[k] = err
 		}
 	}
 	return errs
 }
 
+// common rule
+// for string, params must number-number
+//
 func (this *normalRule) Check(data string) (Err error) {
-	if this.params == "" {
-		Err = errors.New("rule error: params wrong of rule")
-		return
+	//if this.params == "" {
+	//	return errors.New("rule error: params wrong of rule")
+	//}
+	// split rule
+	// 先后规则顺序
+	rules := strings.Split(this.rule,",")
+	for _,v := range rules{
+		switch v {
+		case "len":
+			//字符串，根椐params判断长度的最大值和最小值
+			lg := len(data)
+			args := strings.Split(this.params, "-")
+			min, _ := strconv.Atoi(args[0])
+			max, _ := strconv.Atoi(args[1])
+			switch {
+			case lg < min || lg > max:
+				return errors.New(fmt.Sprintln(this.trans, "长度在", min, "与", max, "之间"))
+			default:
+				return nil
+			}
+		case "required":
+			if data == "" {
+				return errors.New(fmt.Sprintln(this.trans, "为必填项"))
+			}
+		case "phone":
+			if b, _ := regexp.MatchString(`^(1[3|4|5|8][0-9]\d{4,8})$`, data); !b {
+				return errors.New(fmt.Sprintln("手机号格式非法"))
+			}
+		case "email":
+			if b, _ := regexp.MatchString(`^([\w\.\_]{2,10})@(\w{1,}).([a-z]{2,4})$`, data); !b {
+				return errors.New(fmt.Sprintln("邮箱格式非法"))
+			}
+		case "number":
+			//判断是否整数数字
+			//判断最大值和最小值是否在params指定的范围
+		case "list":
+			//判断值是否在params指定的列表
+		case "regular":
+			//是否符合正则表达式
+		default:
+			return errors.New(fmt.Sprintf("rule error: not support of rule=%s", this.rule))
+		}
 	}
-	switch this.rule {
-	case "string":
-		Err = errors.New("范围判断")
-		//字符串，根椐params判断长度的最大值和最小值
-	case "number":
-		//判断是否整数数字
-		//判断最大值和最小值是否在params指定的范围
-	case "list":
-		//判断值是否在params指定的列表
-	case "regular":
-		//是否符合正则表达式
-	default:
-		Err = errors.New(fmt.Sprintf("rule error: not support of rule=%s", this.rule))
-	}
-	return
+	return nil
 }
 
 type myRuler struct {
@@ -106,48 +144,3 @@ func (this *myRuler) Check(data string) (Err error) {
 
 //添加规则
 //validator.AddExtRule("name", &myRuler{})
-
-/*常用验证*/
-//正确返回nil,字符串验证,验证规则
-func CheckRegular(value string) interface{} {
-	//反射得到变量名
-	var info interface{}
-
-	switch {
-	//手机号
-	case strings.Contains(value, "phone"):
-		if b, _ := regexp.MatchString(`^(1[3|4|5|8][0-9]\d{4,8})$`, value); !b {
-			return lib.MapPhone
-		}
-	//邮箱
-	case strings.Contains(value, "email"):
-		if b, _ := regexp.MatchString(`^([\w\.\_]{2,10})@(\w{1,}).([a-z]{2,4})$`, value); !b {
-			return lib.MapEmail
-		}
-	}
-	return info
-}
-
-//正确返回nil,必填验证,验证规则
-//value:值,name:参数名,validator:验证方式
-func CheckValidator(value, name, validator string) interface{} {
-	var info interface{}
-
-	switch validator {
-	//必填项
-	case "required":
-		if value == "" {
-			info = lib.GetMapData(lib.CodeRequired, name+"-->必填项")
-		}
-	}
-	return info
-}
-
-//map[string][]interface{}
-func CheckValueMapArray(args map[string][]string) interface{} {
-	var info interface{}
-	for _, v := range args {
-		info = CheckRegular(v[0])
-	}
-	return info
-}
--- validator/validator_test.go
+++ validator/validator_test.go
@@ -5,17 +5,13 @@ import (
 	"testing"
 )
 
-// test size
+// test validator
 func TestValidator(t *testing.T) {
 	var maps = map[string]string{"name":"qwertyuiopasdfghjklzx"}
 	val := NewValidator(maps) //将要检查的数据字典传入，生成Validator对象
-	val.AddRule("name","string","2-5",true) //对字段name添加规则： 2-5个字符长度，必填
-	val.AddRule("sport","list","football,swim",false) //对字段sport添加规则： 值需在列表中（football，swim),非必填
+	val.AddRule("name", "用户名","required,len","0-5") //对字段name添加规则： 2-5个字符长度，必填
+	//val.AddRule("sport","list","football,swim",false) //对字段sport添加规则： 值需在列表中（football，swim),非必填
 
-	if err:= val.Check(); err !=nil{
-		//检查不通过，处理错误
-		fmt.Println(err)
-		return
-	}
+	fmt.Println(val.CheckInfo("用户名"))
 }
 
