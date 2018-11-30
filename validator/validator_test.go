0e4bdf4e232cdcbdf0f0c429aede33f07be97735
Dreamlu <3147460271@qq.com>
2018-11-30 14:04:55 +0800

    map[string]string to map[string][]string,改为支持原生go web

--- validator/validator_test.go
+++ validator/validator_test.go
@@ -7,7 +7,8 @@ import (
 
 // test validator
 func TestValidator(t *testing.T) {
-	var maps = map[string]string{"name":"qwertyuiopasdfghjklzx"}
+	var maps = make(map[string][]string)
+	maps["name"] = append(maps["name"], "qwertyuiopasdfghjklzx")
 	val := NewValidator(maps) //将要检查的数据字典传入，生成Validator对象
 	val.AddRule("name", "用户名","required,len","0-5") //对字段name添加规则： 2-5个字符长度，必填
 	//val.AddRule("sport","list","football,swim",false) //对字段sport添加规则： 值需在列表中（football，swim),非必填
