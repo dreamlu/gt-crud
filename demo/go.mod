2ab36cae2f2c1e955c0a800a977145c234119abd
Dreamlu <3147460271@qq.com>
2019-01-24 15:32:38 +0800

    update

--- demo/go.mod
+++ demo/go.mod
@@ -1,1 +1,7 @@
 module demo
+
+require (
+	github.com/casbin/casbin v1.8.0
+	github.com/dreamlu/deercoder-gin v1.1.17
+	github.com/gin-gonic/gin v1.3.0
+)
--- demo/routers/router.go
+++ demo/routers/router.go
@@ -7,7 +7,6 @@ import (
 	"github.com/dreamlu/deercoder-gin/util/lib"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"regexp"
 	"strings"
 )
 
@@ -25,6 +24,14 @@ func SetRouter() *gin.Engine {
 	// load the casbin model and policy from files, database is also supported.
 	//e := casbin.NewEnforcer("conf/authz_model.conf", "conf/authz_policy.csv")
 	//router.Use(authz.NewAuthorizer(e))
+
+	//cookie session
+	//store := cookie.NewStore([]byte("secret"))
+	//router.Use(sessions.Sessions("mysession", store))
+
+	//redis session
+	//store, _ := redis.NewStore(10, "tcp", "localhost:6379", "", []byte("secret"))
+	//router.Use(sessions.Sessions("mysession", store))
 
 	// Ping test
 	router.GET("/ping", func(c *gin.Context) {
@@ -96,8 +103,8 @@ func SetRouter() *gin.Engine {
 func CheckLogin() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		path := c.Request.URL.String()
-		if !strings.Contains(path, "login") && !strings.Contains(path, "/captcha/getkey") && !strings.Contains(path, "/captcha/showimage") {
-			_, err := c.Cookie("role_id")
+		if !strings.Contains(path, "login") && !strings.Contains(path, "/static/file") {
+			_, err := c.Cookie("uid")	// may be use session
 			if err != nil {
 				c.Abort()
 				c.JSON(http.StatusOK, lib.MapNoToken)
@@ -115,31 +122,31 @@ func CheckLogin() gin.HandlerFunc {
 //	}
 //}
 
-/*跨域解决方案,待完善,建议nginx 解决*/
-func CorsMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		method := c.Request.Method
-		origin := c.Request.Header.Get("Origin")
-		var filterHost = [...]string{"http://localhost.*", "http://192.168.31.173:3000"}
-		// filterHost 做过滤器，防止不合法的域名访问
-		var isAccess = false
-		for _, v := range filterHost {
-			match, _ := regexp.MatchString(v, origin)
-			if match {
-				isAccess = true
-			}
-		}
-		if isAccess {
-			// 核心处理方式
-			c.Header("Access-Control-Allow-Origin", "*")
-			c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-			c.Header("Access-Control-Allow-Methods", "GET, OPTIONS, POST, PUT, DELETE")
-			c.Set("content-type", "application/json")
-		}
-		//放行所有OPTIONS方法
-		if method == "OPTIONS" {
-			c.JSON(http.StatusOK, "Options Request!")
-		}
-		c.Next()
-	}
-}
+///*跨域解决方案,待完善,建议nginx 解决*/
+//func CorsMiddleware() gin.HandlerFunc {
+//	return func(c *gin.Context) {
+//		method := c.Request.Method
+//		origin := c.Request.Header.Get("Origin")
+//		var filterHost = [...]string{"http://localhost.*", "http://192.168.31.173:3000"}
+//		// filterHost 做过滤器，防止不合法的域名访问
+//		var isAccess = false
+//		for _, v := range filterHost {
+//			match, _ := regexp.MatchString(v, origin)
+//			if match {
+//				isAccess = true
+//			}
+//		}
+//		if isAccess {
+//			// 核心处理方式
+//			c.Header("Access-Control-Allow-Origin", "*")
+//			c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+//			c.Header("Access-Control-Allow-Methods", "GET, OPTIONS, POST, PUT, DELETE")
+//			c.Set("content-type", "application/json")
+//		}
+//		//放行所有OPTIONS方法
+//		if method == "OPTIONS" {
+//			c.JSON(http.StatusOK, "Options Request!")
+//		}
+//		c.Next()
+//	}
+//}
