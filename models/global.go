b9ff2413e52b9ff550eae3eb3794730fa79ab17e
dreamlu <3147460271@qq.com>
2020-05-11 13:13:53 +0800

    up version

--- models/global.go
+++ models/global.go
@@ -9,7 +9,7 @@ type IDCom struct {
 // 通用模型
 type ModelCom struct {
 	IDCom
-	Createtime time.CTime `gorm:"type:datetime;DEFAULT:CURRENT_TIMESTAMP" json:"createtime"` // 创建时间自动生成
+	CreateTime time.CTime `gorm:"type:datetime;DEFAULT:CURRENT_TIMESTAMP" json:"create_time"` // 创建时间自动生成
 }
 
 // 多账号, 如果不需要多账号, 注释掉AdminID即可(ps: 为了简化部署,可直接在util/db/db.go中加入初始化appid applet账号信息)
