01ede9d63535104b523c5f5b65f6ddca092f3443
dreamlu <3147460271@qq.com>
2020-08-17 17:53:58 +0800

    gin全局异常捕获

--- routers/router.go
+++ routers/router.go
@@ -10,6 +10,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
+	"runtime/debug"
 	"strings"
 )
 
@@ -29,6 +30,7 @@ func SetRouter() *gin.Engine {
 
 	// 过滤器
 	router.Use(Filter())
+	router.Use(Recover)
 	//权限中间件
 	// load the casbin model and policy from files, database is also supported.
 	//e := casbin.NewEnforcer("conf/authz_model.conf", "conf/authz_policy.csv")
@@ -113,6 +115,31 @@ func Filter() gin.HandlerFunc {
 			_ = ca.Set(token, cam)
 		}
 	}
+}
+
+// 异常捕获
+func Recover(c *gin.Context) {
+	defer func() {
+		if r := recover(); r != nil {
+			//打印错误堆栈信息
+			//log.Printf("panic: %v\n", r)
+			debug.PrintStack()
+			//封装通用json返回
+			//c.JSON(http.StatusOK, Result.Fail(errorToString(r)))
+			//Result.Fail不是本例的重点，因此用下面代码代替
+			ss := strings.Split(string(debug.Stack()), "\n\t")
+			res := make(map[string]string)
+			for _, v := range ss {
+				ks := strings.Split(v, "\n")
+				res[ks[0]] = ks[1]
+			}
+			c.JSON(http.StatusOK, result.GetError(res))
+			//终止后续接口调用，不加的话recover到异常后，还会继续执行接口里后续代码
+			c.Abort()
+		}
+	}()
+	//加载完 defer recover，继续后续接口调用
+	c.Next()
 }
 
 // 处理跨域请求,支持options访问
